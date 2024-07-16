9821d4371e6118862ee67c3783d78769d042c9e3
Nitish Mandal <138323760+mandalnitish01@users.noreply.github.com>
2024-07-17 00:33:04 +0530

    Add files via upload

---
+++ TO-DO-LIST/script.js
@@ -0,0 +1,72 @@
+const inputBox = document.getElementById('inputBox');
+const addBtn = document.getElementById('addBtn');
+const todoList = document.getElementById('todoList');
+
+let editTodo = null;
+// Function to add todo
+const addTodo = () => {
+    const inputText = inputBox.value.trim();
+    if (inputText.length <= 0) {
+        alert("You must write something in your to do");
+        return false;
+    }
+    
+
+    if (addBtn.value === "Edit") {
+     
+        editTodo.target.previousElementSibling.innerHTML = inputText;
+        addBtn.value = "Add";
+        inputBox.value = "";
+    }
+    else {
+        //Creating p tag
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        p.innerHTML = inputText;
+        li.appendChild(p);
+
+
+        // Creating Edit Btn
+        const editBtn = document.createElement("button");
+        editBtn.innerText = "Edit";
+        //create a class for  edit in css. you can give this
+        editBtn.classList.add("btn", "editBtn");
+        li.appendChild(editBtn);
+
+        // Creating Delete Btn
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Remove";
+        deleteBtn.classList.add("btn", "deleteBtn");
+        li.appendChild(deleteBtn);
+
+        todoList.appendChild(li);
+        inputBox.value = "";
+
+        saveLocalTodos(inputText);
+    }
+}
+
+// Function to update : (Edit/Delete) todo
+const updateTodo = (e) => {
+    if (e.target.innerHTML === "Remove") {
+        todoList.removeChild(e.target.parentElement);
+        addBtn.value = "Add";
+        inputBox.value = "";
+       
+    }
+
+    if (e.target.innerHTML === "Edit") {
+        inputBox.value = e.target.previousElementSibling.innerHTML;
+        inputBox.focus();
+        addBtn.value = "Edit";
+        editTodo = e;
+    }
+}
+
+
+
+
+
+
+addBtn.addEventListener('click', addTodo);
+todoList.addEventListener('click', updateTodo);
